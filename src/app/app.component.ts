042440317b9e97be65500dd10a0cf206e0ad6841
ilaloiv <saint_halo@hotmail.com>
2020-10-23 12:22:03 -0500

    Mi primer respaldo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Link } from './link/link.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'votes';
+  links: Link[];
+
+  constructor(){
+    this.links = [
+      new Link('Angular','http://angular.io',100),
+      new Link('Google','http://google.com',40),
+      new Link('Git','http://github.com/',90),
+    ];
+    console.log(this.links);
+  }
+  
+  addLink(title: HTMLInputElement, link: HTMLInputElement){
+    this.links.push(new Link(title.value, link.value));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
 }
